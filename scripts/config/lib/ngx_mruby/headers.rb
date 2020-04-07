f961bedab7d583f79b748f465c1a805e5157e3c3
Bas <bas@feedbackfruits.com>
2020-04-07 15:46:25 +0200

    debug

--- scripts/config/lib/ngx_mruby/headers.rb
+++ scripts/config/lib/ngx_mruby/headers.rb
@@ -11,6 +11,9 @@ uri    = req.var.uri
 if config["headers"]
   config["headers"].to_a.reverse.each do |route, header_hash|
     if Regexp.compile("^#{NginxConfigUtil.to_regex(route)}$") =~ uri
+      req.headers_out.all.keys.each do |k|
+        Nginx.echo "#{k}: #{req.headers_out[k]}"
+      end
       header_hash.each do |key, value|
         # value must be a string
         req.headers_out[key] = value.to_s unless req.headers_out['status'] == 404
